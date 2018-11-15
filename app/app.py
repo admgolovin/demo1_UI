d54c32e1d1a6804401355c700654fbc40c9ef7ef
nitroboy <nitroboy@duty-linux1.zeo.lcl>
2018-11-15 07:41:52 +0200

    initial release of web app

---
+++ app/app.py
@@ -0,0 +1,56 @@
+import flask
+import requests
+import json
+
+POST_SERVICE_URL = ""
+VIEW_SERVICE_URL = ""
+app = flask.Flask(__name__)
+SEND_DATA_FMT = {"Departament":
+			{"name": None},
+		"Team": {"depart_id": None: int,
+			"name": None: str,
+			"manager_id": None: int},
+		"Employee": {"name": None,
+			"sname": None,
+			"exp": None,
+			"position": None,
+			"salary": None,
+			"coefficient": None}}
+
+
+def get_info():
+	reponse = requests.get(VIEW_SERVICE_URL)
+	return response.json()
+
+
+def send_info(info: dict):
+	info = json.dumps(info)
+	response = requests.post(POST_SERVICE_URL, data=info)
+
+
+@app.route("/")
+def index():
+	return flask.render_template("index.html")
+
+
+@app.route("/give-salary")
+	repsonse = requests.get(VIEW_SERVICE_URL)
+
+
+@app.route("/create-departament")
+def create_departament():
+	pass
+
+
+@app.route("/create_team")
+def create_team():
+	pass
+
+
+@app.route("/hire_employee")
+def hire_employee():
+	pass
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
